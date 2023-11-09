04c081e65fb6a558a4fe56d45abef834dbd765e4
Aleikson <aleiksonsilva@hotmail.com>
2023-11-08 21:05:47 -0300

    new get

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import { CreateCourse } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
+/* app.get("/", (request, response) => {
   return response.json({ message: "Hello World" });
-});
+}); */
+app.get('/', CreateCourse)
 
 app.listen(2222);
