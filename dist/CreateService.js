9ce765ed7322d7e63fc68e7caa4de54080065419
Aleikson <aleiksonsilva@hotmail.com>
2023-11-09 22:32:36 -0300

    Clean cache & new project

--- dist/CreateService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateService {
-    execute(name, duration, educator) {
-        console.log(name, duration, educator);
-    }
-}
-exports.default = new CreateService();
