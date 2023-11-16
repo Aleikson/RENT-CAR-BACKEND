f72e64b5d670ed0517b0ab281307a4ac37017d74
Aleikson <aleiksonsilva@hotmail.com>
2023-11-16 01:36:51 -0300

    Changes on post & get

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,23 @@
-import { Router } from 'express';
-import { Category } from '../model/Category';
+import { Router, response } from 'express';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  Object.assign(category, {
-    name,
-    description,
-    create_at: new Date(),
-  })
- 
-  categories.push(category);
+  createCategoryService.execute({ name, description });
 
-  return response.status(201).json({category});
+  return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (resquest, response) => {
+  const all = categoriesRepository.list();
+  return response.json(all);
 });
 
 export { categoriesRoutes };
