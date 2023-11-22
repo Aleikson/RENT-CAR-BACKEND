cec851bd84b6cec7fb24c713c7dcd0b1f9bb8ec0
Aleikson <aleiksonsilva@hotmail.com>
2023-11-21 22:20:07 -0300

    New route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,15 @@
-import { Router, response } from 'express';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { Router } from 'express';
+import { createCategoryController } from '../modules/cars/useCases/createCategory/';
+import { listCategoriesController } from '../modules/cars/useCases/listCategory';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
-categoriesRoutes.get('/', (resquest, response) => {
-  const all = categoriesRepository.list();
-  return response.json(all);
+categoriesRoutes.get('/', (request, response) => {
+return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
