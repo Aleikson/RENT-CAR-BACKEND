ab808c3da5e396122f951a34908eb8ef78e74921
Aleikson <aleiksonsilva@hotmail.com>
2023-11-24 23:30:31 -0300

    Added implementations

---
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -0,0 +1,41 @@
+import { Category } from '../../model/Category';
+import {
+  ICategoriesRepository,
+  IcreateCategoryDTO,
+} from '../ICategoriesRepository';
+
+export class CategoriesRepository implements ICategoriesRepository {
+  private categories: Category[];
+
+  private static INSTACE: CategoriesRepository;
+
+  constructor() {
+    this.categories = [];
+  }
+
+  public static getInstace(): CategoriesRepository {
+    if (!CategoriesRepository.INSTACE) {
+      CategoriesRepository.INSTACE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTACE
+  }
+
+  create({ name, description }: IcreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      create_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+  list(): Category[] {
+    return this.categories;
+  }
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name == name);
+    return category;
+  }
+}
