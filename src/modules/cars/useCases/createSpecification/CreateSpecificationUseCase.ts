127d4edf6bad4760d78500ff67446ce0e88456c7
Aleikson <aleiksonsilva@hotmail.com>
2023-11-24 23:29:30 -0300

    New useCase

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,22 @@
+import { ISpecificationRepository } from '../../repositories/ISpecificationRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+export class CreateSpecificationUseCase {
+  constructor(private specificationRepository: ISpecificationRepository) {}
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExist = this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExist) {
+      throw new Error('Specification alredy exist');
+    }
+
+    this.specificationRepository.create({
+      name,
+      description,
+    });
+  }
+}
