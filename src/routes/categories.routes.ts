b2099cd1a3db700b6561a5d3cb9efee6a581c3e2
Aleikson <aleiksonsilva@hotmail.com>
2023-11-27 03:57:47 -0300

    Added multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 
 const categoriesRoutes = Router();
+
+const upload = multer({ dest: './tmp' });
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -10,6 +14,11 @@ categoriesRoutes.post('/', (request, response) => {
 
 categoriesRoutes.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  console.log('Received file:', request.file);
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
